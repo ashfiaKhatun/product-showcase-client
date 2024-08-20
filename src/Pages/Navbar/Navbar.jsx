1b2f743c546da9eaade3aa829bfa54ab0df57bbf
ashfiaKhatun <ashfia1204@gmail.com>
2024-08-20 13:38:19 +0600

    navbar added

--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -1,16 +1,44 @@
+import { FaSearch } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
 
-    const list = <>
-        <li><NavLink to='/'>Home</NavLink></li>
-        <li><NavLink to='/signin'>Sign In</NavLink></li>
+    const lists = <>
+        <li><NavLink to="/">Home</NavLink></li>
+
+        <p className="ml-4 mr-2 mt-2 text-base">Filter By: </p>
+        <li className="ml-2 flex">
+            <div className="p-0 flex flex-col md:flex-row gap-4 md:gap-2 mb-2 mt-2 md:mb-0 md:mt-0">
+                <select className="select select-bordered h-10">
+                    <option disabled selected>Brand</option>
+                    <option>Han Solo</option>
+                    <option>Greedo</option>
+                </select>
+                <select className="select select-bordered h-10">
+                    <option disabled selected>Category</option>
+                    <option>Han Solo</option>
+                    <option>Greedo</option>
+                </select>
+                <select className="select select-bordered h-10">
+                    <option disabled selected>Price</option>
+                    <option>Han Solo</option>
+                    <option>Greedo</option>
+                </select>
+            </div>
+        </li>
+
+        <li className="ml-2 flex">
+            <div className="p-0 relative">
+                <input type="text" placeholder="Search Here" className="input input-bordered w-48"/>
+                <FaSearch className="absolute right-2 md:right-4 "></FaSearch>
+            </div>
+        </li>
     </>
 
 
     return (
         <div>
-            <div className="navbar bg-base-100">
+            <div className="navbar bg-base-200">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -30,16 +58,14 @@ const Navbar = () => {
                         <ul
                             tabIndex={0}
                             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                            {list}
-
+                            {lists}
                         </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                    <a className="btn btn-ghost text-xl">E-Commerce</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        {list}
-
+                        {lists}
                     </ul>
                 </div>
                 <div className="navbar-end">
